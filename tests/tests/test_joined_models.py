33e8455c77c5f2cd1b18eb098f578e53a44621a3
Matthew Schinckel <matt@schinckel.net>
2018-11-13 19:46:41 +1030

    Auto-require db access.

--- tests/tests/test_joined_models.py
+++ tests/tests/test_joined_models.py
@@ -1,15 +1,11 @@
-import pytest
-
 from ..models import Address, Person
 
 
-@pytest.mark.django_db
 def test_joined_lookup():
     assert len(Address.objects.filter(person__name='foo')) == 0
     assert len(Address.objects.filter(person__name__icontains='foo')) == 0
 
 
-@pytest.mark.django_db
 def test_select_related():
     person = Person.objects.create(first_name='Foo', last_name='Bar')
     Address.objects.create(person=person)
--- tests/tests/test_simplest_expression.py
+++ tests/tests/test_simplest_expression.py
@@ -1,55 +1,44 @@
 from ..models import Person
 
-import pytest
 
-
-@pytest.mark.django_db
 def test_computed_field_exists_and_can_be_queried():
     assert not Person.objects.filter(name='foo bar').exists()
 
 
-@pytest.mark.django_db
 def test_values_query_result_includes_column():
     Person.objects.create(first_name='Foo', last_name='Bar')
     assert 'name' in Person.objects.values()[0]
 
 
-@pytest.mark.django_db
 def test_create_works():
     Person.objects.create(first_name='Foo', last_name='Bar')
 
 
-@pytest.mark.django_db
 def test_computed_field_is_set_on_object():
     Person.objects.create(first_name='Foo', last_name='Bar')
     assert Person.objects.get().name == 'Foo Bar'
 
 
-@pytest.mark.django_db
 def test_computed_field_is_set_on_values():
     Person.objects.create(first_name='Foo', last_name='Bar')
     assert Person.objects.values('name')[0] == {'name': 'Foo Bar'}
 
 
-@pytest.mark.django_db
 def test_filter_on_computed_field():
     assert not Person.objects.filter(name='Foo Bar').exists()
     Person.objects.create(first_name='Foo', last_name='Bar')
     assert Person.objects.filter(name='Foo Bar').exists()
 
 
-@pytest.mark.django_db
 def test_filter_transform_on_computed_field():
     Person.objects.create(first_name='Foo', last_name='Bar')
     assert Person.objects.filter(name__icontains='foo').exists()
 
 
-@pytest.mark.django_db
 def test_cascading_field():
     Person.objects.create(first_name='Foo', last_name='Bar')
     assert Person.objects.filter(lower_name='foo bar').exists()
 
 
-@pytest.mark.django_db
 def test_ordering():
     assert len(Person.objects.order_by('name')) == 0
