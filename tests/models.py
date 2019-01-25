b3a452bc24e663709d9e5001c8e28e24e29c9d4c
Matthew Schinckel <matt@schinckel.net>
2019-01-25 14:10:07 +1030

    Remove unused import.

--- tests/models.py
+++ tests/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.expressions import Value, ExpressionWrapper
+from django.db.models.expressions import Value
 from django.db.models.functions import Concat, Lower
 
 from computed_field.fields import ComputedField
